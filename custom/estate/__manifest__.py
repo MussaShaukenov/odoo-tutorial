b11519ca4a5ee9f542e77097408117ae8c314c4e
Mussa Shaukenov <mshaukenov@gmail.com>
2022-09-19 16:52:01 +0600

    Chapter 7 finished

--- custom/estate/__manifest__.py
+++ custom/estate/__manifest__.py
@@ -1,12 +1,20 @@
 {
     'name': 'Real Estate',
     'version': '1.0',
-    'description': "",
+    'description': "Real Estate app",
+    'category': 'Custom',
+    'website': 'https://www.youtube.com/?themeRefresh=1',
     'depends': [
         'base'
     ],
     'data': [
-        'security/ir.model.access.csv'
+        'security/ir.model.access.csv',
+
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml'
     ],
-    'license': 'LGPL-3',
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'license': 'LGPL-3'
 }
--- custom/estate/models/estate.py
+++ custom/estate/models/estate.py
@@ -1,20 +1,34 @@
 from odoo import models, fields
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
 
 
 class Estate(models.Model):
+    # name of the module
     _name = 'estate.property'
-    _description = ""
+    _description = "estate property model"
+
+    date_in_three_months = datetime.now() + relativedelta(months=3)
 
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    data_availability = fields.Date()
+    data_availability = fields.Date(copy=False, default=date_in_three_months)
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
     garden_area = fields.Integer()
     garden_orientation = fields.Selection([("north", "North"), ("east", "East"), ("south", "South"), ("west", "West")])
+    state = fields.Selection(
+        [("new", "New"), ("offer_received", "Offer Received"), ("offer_accepted", "Offer Accepted"), ("sold", "Sold"),
+         ("canceled", "Canceled"), ],
+        string="Status",
+        required=True,
+        copy=False,
+        default="New",
+        )
+    active = fields.Boolean("Active", default=True)
